ca62900a7e89d680cff39fcbe1dd7918aec70845
Wendee Postre <pendeewostre@gmail.com>
2023-05-20 11:49:27 +0800

    Profile Update

--- app/src/main/java/com/example/dca/ProfileActivity.java
+++ app/src/main/java/com/example/dca/ProfileActivity.java
@@ -8,14 +8,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    TextView profileName, profileEmail, profileUsername, profilePassword;
+    TextView titleName, titleUsername;
+    Button editProfile;
     DrawerLayout drawerlayout;
     ImageView menu;
     LinearLayout home, profile, cart, settings, about, logout;
@@ -34,7 +45,37 @@ public class ProfileActivity extends AppCompatActivity {
         settings = findViewById(R.id.settings);
         about = findViewById(R.id.about);
         logout = findViewById(R.id.logout);
+        profileName = findViewById(R.id.profileName);
+        profileEmail = findViewById(R.id.profileEmail);
+        profileUsername = findViewById(R.id.profileUsername);
+        profilePassword = findViewById(R.id.profilePassword);
+        titleName = findViewById(R.id.titleName);
+        titleUsername = findViewById(R.id.titleUsername);
+        editProfile = findViewById(R.id.editButton);
 
+        showAllUserData();
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                passUserData();
+            }
+        });
+    }
+    public void showAllUserData(){
+        Intent intent = getIntent();
+        String nameUser = intent.getStringExtra("name");
+        String emailUser = intent.getStringExtra("email");
+        String usernameUser = intent.getStringExtra("username");
+        String passwordUser = intent.getStringExtra("password");
+        titleName.setText(nameUser);
+        titleUsername.setText(usernameUser);
+        profileName.setText(nameUser);
+        profileEmail.setText(emailUser);
+        profileUsername.setText(usernameUser);
+        profilePassword.setText(passwordUser);
+    }
+    public void passUserData(){
+        String userUsername = profileUsername.getText().toString().trim();
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,12 +129,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
     }
-
     public static void openDrawer(DrawerLayout drawerLayout){
         drawerLayout.openDrawer(GravityCompat.START);
     }
@@ -114,3 +150,5 @@ public class ProfileActivity extends AppCompatActivity {
         closeDrawer(drawerlayout);
     }
 }
+
+
