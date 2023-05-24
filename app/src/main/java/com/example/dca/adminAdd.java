2761007aae4117f1c68127981bfd2b3e76a88c7b
Ibias, Norien Joyce N. _ BSIT-2A <98938806+Yournn20@users.noreply.github.com>
2023-05-25 01:32:05 +0800

    Admin darkmode

--- app/src/main/java/com/example/dca/adminAdd.java
+++ app/src/main/java/com/example/dca/adminAdd.java
@@ -5,11 +5,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -28,7 +30,6 @@ public class adminAdd extends AppCompatActivity {
     Button saveButton;
     EditText uploadName, uploadDesc, uploadPrice;
     String imageURL;
-    Spinner spinner;
     Uri uri;
 
     @Override
@@ -41,11 +42,6 @@ public class adminAdd extends AppCompatActivity {
         uploadDesc = findViewById(R.id.uploadDesc);
         uploadPrice = findViewById(R.id.uploadPrice);
         saveButton = findViewById(R.id.btnadd);
-        spinner = findViewById(R.id.spinner);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_values, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
         uploadImage.setOnClickListener(view -> {
             Intent photoPicker = new Intent(Intent.ACTION_PICK);
@@ -121,25 +117,43 @@ public class adminAdd extends AppCompatActivity {
         String name = uploadName.getText().toString();
         String desc = uploadDesc.getText().toString();
         String price = uploadPrice.getText().toString();
-        String selectedSpinnerItem = spinner.getSelectedItem().toString();
         DataClass dataClass = new DataClass(name, desc, price, imageURL);
 
-        FirebaseDatabase.getInstance().getReference("products").child(selectedSpinnerItem)
-                .setValue(dataClass).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(adminAdd.this, "Saved", Toast.LENGTH_SHORT).show();
-                            finish();
-                        } else {
-                            Toast.makeText(adminAdd.this, "Failed to save data", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(Exception e) {
-                        Toast.makeText(adminAdd.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+        DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference("products");
+
+        productsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long childCount = dataSnapshot.getChildrenCount(); // Get the current number of children
+
+                DatabaseReference childRef = productsRef.child(String.valueOf(childCount + 1)); // Increment the child key by 1
+
+                childRef.setValue(dataClass)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(adminAdd.this, "Product Information Saved", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                } else {
+                                    Toast.makeText(adminAdd.this, "Failed to save product information", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(Exception e) {
+                                Toast.makeText(adminAdd.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle potential error
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/dca/adminUpdate.java
+++ app/src/main/java/com/example/dca/adminUpdate.java
@@ -1,8 +1,8 @@
 package com.example.dca;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class adminUpdate extends AppCompatActivity {
 
@@ -10,5 +10,6 @@ public class adminUpdate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_update);
+
     }
 }
